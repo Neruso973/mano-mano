cb80044c46ced03b3c6f4763d6e55c5236b47451
lfvrarnaud <lfvrarnaud@gmail.com>
2022-01-19 17:00:32 +0100

    wip

---
+++ front-end/src/components/Radar.jsx
@@ -0,0 +1,11 @@
+import photo from "../asset/img/IMG_20220119_133444.jpg";
+
+const Radar = () => {
+  return (
+    <div className="cadre_radar">
+      <img src={photo} alt="radar" />
+    </div>
+  );
+};
+
+export default Radar;
