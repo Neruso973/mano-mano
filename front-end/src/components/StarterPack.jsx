93fcdd58d7613eb61669a23f1eacd3be388ca015
Neruso973 <nelson.ntepp@icloud.com>
2022-01-20 17:40:46 +0100

    video

--- front-end/src/components/StarterPack.jsx
+++ front-end/src/components/StarterPack.jsx
@@ -9,7 +9,7 @@ function StarterPack() {
       <div className="flex justify-between bg-white">
         <div className="flex flex-col w-[40%]">
             <h2 className="bg-gradient-to-r from-[#24A6B2] to-[#00ECCD] rounded-xl text-xl my-6  mx-10 p-2 text-white text-center"> Mettre une étagère à niveau ?</h2>
-            <div className="w-[100%] h-[40%] bg-black my-6 mx-8"></div>
+            <iframe className="w-[100%] h-[50%] bg-black my-6 mx-8" src="https://www.youtube.com/embed/c2NoIAvKdjk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
             <img src={level} className="w-[50%] mx-24 my-4"/>
         </div>
         <div className="flex flex-col w-[50%]">
